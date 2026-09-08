1372387d7737168256ef132c1e2a8d49899e5c08
Philipp Enger-Tšižikov <xxestproxx@gmail.com>
2026-09-08 13:53:26 +0300

    Add vite

--- learnfront/index.js
+++
@@ -1,5 +0,0 @@
-console.log("Hello via Bun!");
-import fs from "fs";
-
-let text = fs.readFileSync("./index.js", "utf-8");
-console.log(text);
---
+++ learnfront/src/index.js
@@ -0,0 +1,4 @@
+import math from "./math.js";
+
+let answer = math.add(2, 4);
+console.log(answer);
